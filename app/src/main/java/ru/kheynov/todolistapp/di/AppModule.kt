5dbe1185a0466885bd9d0a2c3e8c63ecce47e4d3
Vladislav Kheynov <lcastory86@gmail.com>
2021-10-31 21:21:02 +0700

    feat: add main app module functionality

--- app/src/main/java/ru/kheynov/todolistapp/di/AppModule.kt
+++ app/src/main/java/ru/kheynov/todolistapp/di/AppModule.kt
@@ -1,4 +1,45 @@
 package ru.kheynov.todolistapp.di
 
-class AppModule {
+import android.app.Application
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import ru.kheynov.todolistapp.feature_todo.data.data_source.TodoDatabase
+import ru.kheynov.todolistapp.feature_todo.data.repository.TodoRepositoryImpl
+import ru.kheynov.todolistapp.feature_todo.domain.repository.TodoRepository
+import ru.kheynov.todolistapp.feature_todo.domain.use_case.DeleteTodo
+import ru.kheynov.todolistapp.feature_todo.domain.use_case.GetTodos
+import ru.kheynov.todolistapp.feature_todo.domain.use_case.TodosUseCases
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideTodoDatabase(app: Application): TodoDatabase {
+        return Room.databaseBuilder(
+            app,
+            TodoDatabase::class.java,
+            TodoDatabase.DATABASE_NAME
+        ).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideTodoRepository(db: TodoDatabase): TodoRepository {
+        return TodoRepositoryImpl(db.todoDao)
+    }
+
+    @Provides
+    @Singleton
+    fun provideTodoUseCases(repository: TodoRepository): TodosUseCases {
+        return TodosUseCases(
+            getTodos = GetTodos(repository),
+            deleteTodo = DeleteTodo(repository)
+        )
+    }
 }
