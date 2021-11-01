145b7076b673454e97da2258567885707d880ea6
Vladislav Kheynov <lcastory86@gmail.com>
2021-11-01 21:55:06 +0700

    refactor: add 'addTodo' to use cases list

--- app/src/main/java/ru/kheynov/todolistapp/feature_todo/domain/use_case/TodosUseCases.kt
+++ app/src/main/java/ru/kheynov/todolistapp/feature_todo/domain/use_case/TodosUseCases.kt
@@ -2,5 +2,6 @@ package ru.kheynov.todolistapp.feature_todo.domain.use_case
 
 data class TodosUseCases(
     val getTodos: GetTodos,
-    val deleteTodo: DeleteTodo
+    val deleteTodo: DeleteTodo,
+    val addTodo: AddTodo
 )
