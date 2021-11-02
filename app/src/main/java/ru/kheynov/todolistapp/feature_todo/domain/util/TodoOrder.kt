fcdfea21b4c35971d70391e39c0b39f764898980
Vladislav Kheynov <lcastory86@gmail.com>
2021-11-02 12:11:15 +0700

    feat: add copy method to keep TodoOrder immutable

--- app/src/main/java/ru/kheynov/todolistapp/feature_todo/domain/util/TodoOrder.kt
+++ app/src/main/java/ru/kheynov/todolistapp/feature_todo/domain/util/TodoOrder.kt
@@ -1,6 +1,13 @@
 package ru.kheynov.todolistapp.feature_todo.domain.util
 
-sealed class TodoOrder(val orderType: OrderType){
-    class Title(orderType: OrderType): TodoOrder(orderType)
-    class Date(orderType: OrderType): TodoOrder(orderType)
+sealed class TodoOrder(val orderType: OrderType) {
+    class Title(orderType: OrderType) : TodoOrder(orderType)
+    class Date(orderType: OrderType) : TodoOrder(orderType)
+
+    fun copy(orderType: OrderType): TodoOrder {
+        return when(this){
+            is Title -> Title(orderType)
+            is Date -> Date(orderType)
+        }
+    }
 }
