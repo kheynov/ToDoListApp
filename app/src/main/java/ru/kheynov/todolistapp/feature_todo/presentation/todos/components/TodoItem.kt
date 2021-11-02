171088db0413d4969d38c471f9b99578fac4e1d3
Vladislav Kheynov <lcastory86@gmail.com>
2021-11-02 12:41:33 +0700

    refactor: todo item ui refactor

--- app/src/main/java/ru/kheynov/todolistapp/feature_todo/presentation/todos/components/TodoItem.kt
+++ app/src/main/java/ru/kheynov/todolistapp/feature_todo/presentation/todos/components/TodoItem.kt
@@ -1,5 +1,7 @@
 package ru.kheynov.todolistapp.feature_todo.presentation.todos.components
 
+import android.content.res.Configuration.UI_MODE_NIGHT_NO
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
@@ -8,18 +10,28 @@ import androidx.compose.material.icons.filled.Delete
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import ru.kheynov.todolistapp.feature_todo.domain.model.Todo
+import ru.kheynov.todolistapp.ui.theme.ToDoListAppTheme
 
-@Preview
+@Preview(name = "Light", uiMode = UI_MODE_NIGHT_NO, showBackground = true)
+@Preview(name = "Dark", uiMode = UI_MODE_NIGHT_YES, showBackground = true)
 @Composable
 fun TodoItemPreview() {
-    TodoItem(todo = Todo("Lorem ipsum dolor sit amet, consectetur adipiscing elit.", false, 1312414)) {
-
+    ToDoListAppTheme {
+        TodoItem(
+            todo = Todo(
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                true,
+                1312414
+            ),
+            onDeleteClick = {},
+            onChecked = {}
+        )
     }
 }
 
@@ -29,7 +41,8 @@ fun TodoItem(
     todo: Todo,
     modifier: Modifier = Modifier,
     cornerRadius: Dp = 10.dp,
-    onDeleteClick: () -> Unit
+    onDeleteClick: () -> Unit,
+    onChecked: (Boolean) -> Unit
 ) {
     Card(
         modifier = modifier.height(70.dp),
@@ -39,14 +52,16 @@ fun TodoItem(
             modifier = Modifier
                 .fillMaxSize(),
             verticalAlignment = Alignment.CenterVertically
-//                .padding(16.dp)
-//                .padding(end = 32.dp),
-            ) {
-            Spacer(modifier = Modifier.width(10.dp))
+        ) {
+            Spacer(modifier = Modifier.width(18.dp))
             Checkbox(
-                checked = todo.isChecked, onCheckedChange = {}
+                checked = todo.isChecked,
+                onCheckedChange = onChecked,
+                colors = CheckboxDefaults.colors(
+
+                )
             )
-            Spacer(modifier = Modifier.width(10.dp))
+            Spacer(modifier = Modifier.width(18.dp))
             Text(
                 text = todo.title,
                 style = MaterialTheme.typography.h5,
@@ -55,13 +70,18 @@ fun TodoItem(
                 overflow = TextOverflow.Ellipsis,
                 modifier = Modifier.weight(16f)
             )
-            Spacer(modifier = Modifier.height(8.dp))
+            Spacer(modifier = Modifier.height(6.dp))
             IconButton(
                 onClick = onDeleteClick,
                 modifier = Modifier.weight(2f)
             ) {
-                Icon(imageVector = Icons.Default.Delete, contentDescription = "Delete Todo")
+                Icon(
+                    imageVector = Icons.Default.Delete,
+                    contentDescription = "Delete Todo",
+                    tint = Color.Gray
+                )
             }
+            Spacer(modifier = Modifier.width(18.dp))
         }
     }
 }
